6da9bc5ae02be21546ba33b298d94291c68d053f
wj <w1595519431@gmail.com>
2023-12-28 10:57:42 +0800

    feat: 项目初始化

---
+++ src/main/java/com/wj/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.wj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author w
+ * {@code @time:} 10:03
+ * Description: Project Startup Class
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
